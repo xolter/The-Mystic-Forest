d96561e8b42b84dc5217897363ae5a519e41af40
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2017-02-28 00:57:42 +0100

    ajout d'un menu avec des options vides

---
+++ TheMystic/Assets/scripts/MainMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void LOAD_SCENE()
+	{
+		Application.LoadLevel ("ScenePrincipale");
+	}
+	public void QUIT_GAME()
+	{
+		Application.Quit ();
+	}
+	public void BACK_TO_MENU()
+	{
+		Application.LoadLevel ("MainMenu");
+	}
+	public void TO_OPTION()
+	{
+		Application.LoadLevel ("OptionMenu");
+	}
+}
