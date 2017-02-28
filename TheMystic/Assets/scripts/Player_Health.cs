774da65a865eb455a277ec40bb1f44523ef01db7
Marrt1 <martin.saconney@gmail.com>
2017-02-28 16:42:31 +0100

    ajout de hero3 et quelque scripts du main_char

---
+++ TheMystic/Assets/scripts/Player_Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Health : MonoBehaviour
+{
+    public int max_health = 100;
+    public int current_health = 100;
+    public float healthBar_length;
+	// Use this for initialization
+	void Start ()
+    {
+        healthBar_length = Screen.width / 2;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Update_currentHealth(0);	
+	}
+
+    void On_GUI()
+    {
+        GUI.Box(new Rect(10, 10, healthBar_length, 20), current_health + "/" + max_health);
+    }
+
+    public void Update_currentHealth(int n)
+    {
+        current_health += n;
+        healthBar_length = (Screen.width / 2) * (current_health / (float)max_health);
+    }
+}
---
+++ TheMystic/Assets/scripts/character_Atk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_Atk : MonoBehaviour
+{
+    public GameObject Target;
+	// Use this for initialization
+	void Start ()
+    {
+        float damage = 15;
+        float range = 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }	
+	}
+
+    private void Attack()
+    {
+        Enemy
+    }
+}
