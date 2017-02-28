c65a0b982de2bd89e5baa110f4796c9b64cba68d
Bananasplit2021 <maxime.agnola@epita.fr>
2017-03-01 00:32:22 +0100

    camera control

--- TheMystic/Assets/scripts/Camera_controller.cs
+++ TheMystic/Assets/scripts/Camera_controller.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour
-{
+public class Camera_controller : MonoBehaviour {
+
     public Transform target;
     public float lookSmooth = 0.09f;
     public Vector3 offsetFromTarget = new Vector3(0, 6, -8);
@@ -17,7 +17,7 @@ public class CameraControl : MonoBehaviour
     {
         SetCamertaTarget(target);
     }
-    void SetCamertaTarget (Transform t)
+    void SetCamertaTarget(Transform t)
     {
         target = t;
         if (target != null)
@@ -28,7 +28,7 @@ public class CameraControl : MonoBehaviour
             }
             else
             {
-                Debug.LogError("The camera's target must be a character controller!")
+                Debug.LogError("The camera's target must be a character controller!");
             }
         }
         else
@@ -37,7 +37,7 @@ public class CameraControl : MonoBehaviour
         }
     }
 
-    void LateUpdate ()
+    void LateUpdate()
     {
         //moving
         MoveToTarget();
