fcd42081b5e8d9a68bfc9b9145d101725110b957
Bananasplit2021 <maxime.agnola@epita.fr>
2017-02-28 22:19:23 +0100

    avancement de la map

--- TheMystic/Assets/scripts/CharacterController.cs
+++ TheMystic/Assets/scripts/CharacterController.cs
@@ -2,108 +2,109 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+public class CharacterController : MonoBehaviour
+{
 
-	[System.Serializable]
-	public class MoveSettings
-	{
-		public float forwardVel = 12;
-		public float rotateVel = 100;
-		public float jumpvel = 25;
-		public float distToGrounded = 2f;   //askip c la qu'il faut changer mais le tuto etait pas prevu pour terrain non plat (décu)z
-		public LayerMask ground;
-	}
+    [System.Serializable]
+    public class MoveSettings
+    {
+        public float forwardVel = 12;
+        public float rotateVel = 100;
+        public float jumpvel = 25;
+        public float distToGrounded = 2f;   //askip c la qu'il faut changer mais le tuto etait pas prevu pour terrain non plat (décu)z
+        public LayerMask ground;
+    }
 
-	[System.Serializable]
-	public class PhysSettings
-	{
-		public float downAccel = 0.75f;
-	}
+    [System.Serializable]
+    public class PhysSettings
+    {
+        public float downAccel = 0.75f;
+    }
 
-	[System.Serializable]
-	public class InputSettings
-	{
-		public float inputDelay = 0.1f;
-		public string FORWARD_AXIS = "Vertical";
-		public string TURN_AXIS = "Horizontal";
-		public string JUMP_AXIS = "Jump";
-	}
+    [System.Serializable]
+    public class InputSettings
+    {
+        public float inputDelay = 0.1f;
+        public string FORWARD_AXIS = "Vertical";
+        public string TURN_AXIS = "Horizontal";
+        public string JUMP_AXIS = "Jump";
+    }
 
-	public MoveSettings moveSettings = new MoveSettings();
-	public PhysSettings physSettings = new PhysSettings();
-	public InputSettings inputSettings = new InputSettings();
+    public MoveSettings moveSettings = new MoveSettings();
+    public PhysSettings physSettings = new PhysSettings();
+    public InputSettings inputSettings = new InputSettings();
 
-	Vector3 velocity = Vector3.zero;
-	Quaternion targetRotation;
-	Rigidbody rBody;
-	float forwardInput, turnInput, jumpInput;
+    Vector3 velocity = Vector3.zero;
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float forwardInput, turnInput, jumpInput;
 
-	public Quaternion TargetRotation
-	{
-		get{ return targetRotation; }
-	}
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
 
-	bool Grounded()
-	{
-		return Physics.Raycast (transform.position, Vector3.down, moveSettings.distToGrounded, moveSettings.ground);
-	}
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, moveSettings.distToGrounded, moveSettings.ground);
+    }
 
-	void Start ()
-	{
-		targetRotation = transform.rotation;
-		if (GetComponent<Rigidbody> ())
-			rBody = GetComponent<Rigidbody> ();
-		else
-			Debug.LogError ("character needs rigidbody");
-		forwardInput = turnInput = jumpInput = 0;
-	}
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+            rBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("character needs rigidbody");
+        forwardInput = turnInput = jumpInput = 0;
+    }
 
-	void GetInput()
-	{
-		forwardInput = Input.GetAxis (inputSettings.FORWARD_AXIS);
-		turnInput = Input.GetAxis (inputSettings.TURN_AXIS);
-		jumpInput = Input.GetAxisRaw (inputSettings.JUMP_AXIS);
-	}
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis(inputSettings.FORWARD_AXIS);
+        turnInput = Input.GetAxis(inputSettings.TURN_AXIS);
+        jumpInput = Input.GetAxisRaw(inputSettings.JUMP_AXIS);
+    }
 
-	void Update ()
-	{
-		GetInput ();
-		Turn ();
-	}
+    void Update()
+    {
+        GetInput();
+        Turn();
+    }
 
-	void FixedUpdate()
-	{
-		Run ();
-		Jump ();
+    void FixedUpdate()
+    {
+        Run();
+        //Jump ();
 
-		rBody.velocity = transform.TransformDirection (velocity);
-	}
+        rBody.velocity = transform.TransformDirection(velocity);
+    }
 
-	void Run()
-	{
-		if (Mathf.Abs(forwardInput) > inputSettings.inputDelay)
-		{
-			//move
-			//rBody.velocity = transform.forward * forwardInput * movesettings.forwardVel;
-			velocity.z = moveSettings.forwardVel * forwardInput;
-		}
-		else
-		{
-			//zero velocity
-			//rBody.velocity = Vector3.zero;
-			velocity.z = 0;
-		}
-	}
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputSettings.inputDelay)
+        {
+            //move
+            //rBody.velocity = transform.forward * forwardInput * movesettings.forwardVel;
+            velocity.z = moveSettings.forwardVel * forwardInput;
+        }
+        else
+        {
+            //zero velocity
+            //rBody.velocity = Vector3.zero;
+            velocity.z = 0;
+        }
+    }
 
-	void Turn()
-	{
-		if (Mathf.Abs (turnInput) > inputSettings.inputDelay)
-		{
-			targetRotation *= Quaternion.AngleAxis (moveSettings.rotateVel * turnInput * Time.deltaTime, Vector3.up);
-			transform.rotation = targetRotation;
-		}
-	}
-	void Jump()
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputSettings.inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(moveSettings.rotateVel * turnInput * Time.deltaTime, Vector3.up);
+            transform.rotation = targetRotation;
+        }
+    }
+    /*void Jump()
 	{
 		if (jumpInput > 0 && Grounded())
 		{
@@ -117,5 +118,5 @@ public class CharacterController : MonoBehaviour {
 		{
 			velocity.y -= physSettings.downAccel;
 		}
-	}
+	}*/
 }
