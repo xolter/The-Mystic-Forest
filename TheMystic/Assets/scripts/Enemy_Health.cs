5c0ff9f75f790855df214a1e4d95e6cdb4a1be8b
Marrt1 <martin.saconney@gmail.com>
2017-03-01 11:41:37 +0100

    Ajout des dossiers Spites et Canvas

--- TheMystic/Assets/scripts/Enemy_Health.cs
+++ TheMystic/Assets/scripts/Enemy_Health.cs
@@ -4,42 +4,26 @@ using UnityEngine;
 
 public class Enemy_Health : MonoBehaviour
 {
-    public int max_health = 100;
-    public int current_health = 100;
-    public float healthBar_length;
+    public int maxHealth = 100;
+    public int curHealth = 100;
+    public float healthBarLength;
+    public Transform target;
+
     // Use this for initialization
     void Start()
     {
-        healthBar_length = Screen.width / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Update_currentHealth(0);
     }
 
     void OnGUI()
     {
-        GUI.Box(new Rect(10, 10, healthBar_length, 20), current_health + "/" + max_health);
     }
 
-    public void Update_currentHealth(int n)
+    public void AddjustCurrentHealth(int adj)
     {
-        current_health += n;
-        if (current_health < 0)
-        {
-            current_health = 0;
-        }
-        if (current_health > max_health)
-        {
-            current_health = max_health;
-        }
-        if (max_health < 1)
-        {
-            max_health = 1;
-        }
-
-        healthBar_length = (Screen.width / 2) * (current_health / (float)max_health);
     }
 }
