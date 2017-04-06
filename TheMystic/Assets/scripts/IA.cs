633e3af257b61236986e620126472629fd334c56
Bananasplit2021 <maxime.agnola@epita.fr>
2017-04-06 21:09:37 +0200

    ajout d'un asset + d'une IA basique

---
+++ TheMystic/Assets/scripts/IA.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA : MonoBehaviour
+{
+    public Transform target;
+    public int moveSpeed;
+    public int rotationSpeed;
+    public Transform myTransform;
+    private void Awake()
+    {
+        myTransform = transform;
+    }
+    // Use this for initialization
+    void Start () {
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+        target = go.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+        myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+	}
+}
