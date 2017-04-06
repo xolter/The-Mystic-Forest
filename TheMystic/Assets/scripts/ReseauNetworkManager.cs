fbba3f8ba0d8c4cf7d1a864ce91f2aeed95c8929
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2017-04-07 00:28:42 +0200

    freezepos du pnj, ajout boutons dans le menu pause script reseau

---
+++ TheMystic/Assets/scripts/ReseauNetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ReseauNetworkManager : NetworkManager {
+
+	public void START_HOST()
+	{
+		SetPort ();
+		NetworkManager.singleton.StartHost ();
+	}
+	public void JOIN_GAME()
+	{
+		SetIPAddress ();
+		NetworkManager.singleton.StartClient ();
+	}
+	void SetIPAddress()
+	{
+		string ipAddress = GameObject.Find ("InputFieldIPAddress").transform.FindChild ("Text").GetComponent<Text> ().text;
+		NetworkManager.singleton.networkAddress = ipAddress;
+	}
+	void SetPort()
+	{
+		NetworkManager.singleton.networkPort = 7777;
+	}	
+}
