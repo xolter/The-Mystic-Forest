35ca0424a4cd853b14f1a981a9123f688002b071
Marrt1 <martin.saconney@gmail.com>
2017-04-21 17:53:03 +0200

    LES ZANIMATION SONT LA

--- TheMystic/Assets/scripts/Animation/Player_Animation.cs
+++ TheMystic/Assets/scripts/Animation/Player_Animation.cs
@@ -4,50 +4,28 @@ using UnityEngine;
 
 public class Player_Animation : MonoBehaviour
 {
-    [System.Serializable]
-    public class InputSettings
-    {
-        public float inputDelay = 0.1f;
-        public string FORWARD_AXIS = "Vertical";
-        public string TURN_AXIS = "Horizotal";
-        public string ROTATE_AXIS = "Mouse X";
-        public string JUMP_AXIS = "Jump";
-    }
-
-    public InputSettings inputSettings = new InputSettings();
     public Animator anim;
-    float forwardInput, turnInput, rotateInput, jumpInput;
+    private float inputH;
+    private float inputV;
 
-
-    void Start ()
+    void Start()
     {
         anim = GetComponent<Animator>();
-	}
-	
-	
-	void Update ()
-    {
-        //GetInput();
-        //Animations();
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            anim.Play("Forward", -1, 0f);
-        }
+        anim.SetFloat("inputH", 0);
+        anim.SetFloat("inputV", 0);
     }
 
-    void GetInput()
-    {
-        forwardInput = Input.GetAxis(inputSettings.FORWARD_AXIS);
-        turnInput = Input.GetAxis(inputSettings.TURN_AXIS);
-        rotateInput = Input.GetAxis(inputSettings.ROTATE_AXIS);
-        jumpInput = Input.GetAxisRaw(inputSettings.JUMP_AXIS);
-    }
 
-    void Animations()
+    void Update()
     {
-        if (forwardInput > 0)
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            anim.Play("Forward", -1, 0f);
+            anim.Play("Skill1", -1, 0f);
         }
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+        anim.SetFloat("InputH", inputH);
+        anim.SetFloat("InputV", inputV);
+        Debug.Log(inputH + "~~" +inputV);        
     }
 }
