bb6319fd9311935ebe2abb449b790e8d29cc0608
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2017-04-24 20:43:43 +0200

    gameover simple mais ca marche pas

---
+++ TheMystic/Assets/scripts/GameOverScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScene : MonoBehaviour {
+
+    public Player_Health health;
+    bool GameOverisActive = false;
+
+    void Start()
+    {
+        health = GetComponent<Player_Health>();
+    }
+    void Update()
+    {
+        /*if (health.current_health <= 0)
+        {
+            GameOverisActive = true;
+        }*/
+        if (GameOverisActive)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+        
+    }
+}
