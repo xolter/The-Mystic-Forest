8a0412c7311ae6b15d0846dcb2390b0942bafcfb
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2017-04-25 18:08:34 +0200

    skills, musique chouette avouez + debut camera map

---
+++ TheMystic/Assets/scripts/Menu/SkillsPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillsPanel : MonoBehaviour {
+
+    public GameObject skillPanel;
+    bool isactive = false;
+
+	// Update is called once per frame
+	void Update () {
+        if (isactive)
+        {
+            skillPanel.SetActive(true);
+        }
+        else
+        {
+            skillPanel.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            isactive = !isactive;
+        }
+	}
+}
