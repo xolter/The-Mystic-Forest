8b307d7d401a8ad982854f4d3008c1b0bc12c47c
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2017-04-25 19:11:54 +0200

    minimap ready

---
+++ TheMystic/Assets/scripts/Menu/MinimapCameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+    }
+}
