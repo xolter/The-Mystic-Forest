35f7592046e4d5b0b14300a9436e5d6bb0425f51
Marrt1 <martin.saconney@gmail.com>
2017-06-06 03:54:34 +0200

    ajout de EnemyManager

---
+++ TheMystic/Assets/scripts/Enemy_spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_spawn : MonoBehaviour
+{
+    public GameObject enemy;
+    public Enemy_Health health;
+    //public string tag;
+	void Start ()
+    {               
+        Instantiate(enemy,transform.position, transform.rotation);
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        health = enemy.GetComponent<Enemy_Health>();
+        Debug.Log("spawned");
+    }
+	
+	void Update ()
+    {
+        Debug.Log("cur life [SPAWN]= " + health.current_health);
+        if (health.current_health == 0)
+        {
+            Debug.Log("DEAD");
+            Destroy(enemy, 5f);
+        }
+    }
+}
