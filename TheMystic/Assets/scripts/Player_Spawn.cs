78f2b16b2660377efa2440601d4cfb533e9a11c0
Marrt1 <martin.saconney@gmail.com>
2017-06-13 16:54:30 +0200

    Fix

---
+++ TheMystic/Assets/scripts/Player_Spawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player_Spawn : NetworkBehaviour
+{
+    private NetworkStartPosition spawnpoint;
+	void Start ()
+    {
+		if(isLocalPlayer)
+        {
+            spawnpoint = FindObjectOfType<NetworkStartPosition>();
+        }
+	}
+}
